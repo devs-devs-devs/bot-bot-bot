3d9c9b7d3ef0b920b7ed8f160034328bd48135d7
Ash Taylor <a@zi.vc>
2017-11-30 21:28:23 +0000

    thing

--- src/commands/copypasta.ts
+++ src/commands/copypasta.ts
@@ -1,4 +1,10 @@
+import fs = require('fs');
+import path = require('path');
+
 export class Copypasta {
+
+    private savePath: string = path.resolve(process.cwd(), '../bot-data');
+    private filePath: string = `${this.savePath}/copypasta.json`;
 
     private commands: object = ['cp', 'copypasta'];
 
@@ -6,7 +12,11 @@ export class Copypasta {
 
     constructor() {
         console.log(this.commands, 'loaded');
-        this.copypastas = [];
+        if (!fs.existsSync(this.savePath)) fs.mkdirSync(this.savePath);
+        if (!fs.existsSync(this.filePath)) {
+            fs.writeFileSync(this.filePath, JSON.stringify([]))
+        }
+        this.copypastas = JSON.parse(fs.readFileSync(this.filePath).toString());
     }
 
     reply(params: string, event: object) {
@@ -16,18 +26,28 @@ export class Copypasta {
         const actions = ['add'];
 
         if (actions.indexOf(action) >= 0) {
-            this.copypastas.push(params.substring(params.indexOf(' ')));
+            this.copypastas.push(params.substring(params.indexOf(' ')).trim());
+            this.save();
+            return {
+                text:'Added!'
+            };
         } else {
             // find random copypasta
-            if (!this.copypastas.length) return {
-                text:'No copypastas'
-            };
+            if (!this.copypastas.length) {
+                return {
+                    text:'No copypastas'
+                };
+            }
 
             return {
                 text: this.copypastas[Math.floor(Math.random() * this.copypastas.length)]
             }
         }
 
+    }
+
+    save() {
+        fs.writeFileSync(this.filePath, JSON.stringify(this.copypastas));
     }
 
 }
