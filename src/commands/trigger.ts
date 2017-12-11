2f8e006857276761eaf66282859ea43655743d2a
Ash Taylor <a@zi.vc>
2017-12-11 18:44:41 +0000

    default array thing maybe

--- src/commands/trigger.ts
+++ src/commands/trigger.ts
@@ -2,6 +2,7 @@ import Data from '../services/data';
 import Reply from '../services/reply';
 
 function shuffle(a: any) {
+    a = a || [];
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [a[i], a[j]] = [a[j], a[i]];
