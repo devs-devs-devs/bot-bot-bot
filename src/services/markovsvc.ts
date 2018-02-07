b2778b0ab58b9f8c15112dad9c272eac0ff8371f
Ash Taylor <a@zi.vc>
2018-02-07 14:04:54 +0000

    why not work

--- src/services/markovsvc.ts
+++ src/services/markovsvc.ts
@@ -4,6 +4,8 @@ import * as path from 'path';
 const markov = require('markov');
 
 const markovFile = path.resolve(__dirname, '../../markov.txt');
+console.log('MARKOVFILE', markovFile);
+fs.ensureFileSync(markovFile);
 
 class Markov {
 
@@ -23,6 +25,7 @@ class Markov {
     }
 
     write(str: string = '') {
+        console.log('Writing to file', str);
         fs.appendFileSync(markovFile, str.trim()+"\n");
     }
 
