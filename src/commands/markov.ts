e6bacfcba5c4f8d28c95d6813b7066d4c3b318f9
Ash Taylor <a@zi.vc>
2018-02-07 14:13:13 +0000

    testing markov

--- src/commands/markov.ts
+++ src/commands/markov.ts
@@ -2,6 +2,8 @@ import Command from './command';
 import Reply from '../services/reply';
 import { SlackEvent, SlackMessage } from '../interfaces/slack';
 
+import Markov from '../services/markovsvc';
+
 export class MarkovCommand extends Command {
 
     constructor() {
@@ -16,16 +18,12 @@ export class MarkovCommand extends Command {
 
     reply(body: SlackMessage) {
         const event = body.event as SlackEvent;
-        const { action, params } = this.parseText(event.text);
 
-        const emoji = action[0] === ':' ? action : ':clap:';
-        const sentence = action[0] === ':' ? params : `${action} ${params}`;
-
-        const emphasis = sentence.split(' ').join(` ${emoji} `);
-
-        Reply({
-            text: `_${emphasis}_`
-        }, event);
+        Markov.reply(event.text).then((reply: any) => {
+            Reply({
+                text: reply
+            }, event);
+        });
 
     }
 
